49b2b65f19ba5aae0c9ec8eef279bef4bde8f4b9
yaqed <daniel.wannke@web.de>
2025-02-04 23:43:39 +0100

    feat: new Buttons in training

--- pages/training.vue
+++ pages/training.vue
@@ -1,8 +1,24 @@
 <script setup>
 const answerOptions = ['A', 'B', 'C', 'D', 'E'];
+const showSolution = ref(false);
+
+const toggleSolutionTask = () => {
+  showSolution.value = !showSolution.value;
+  if (showSolution.value) {
+    console.log('Lösung anzeigen');
+  } else {
+    console.log('Aufgabe anzeigen');
+  }
+  //TODO
+};
 
 const sendAnswer = (answer) => {
   console.log('Ausgewählte Antwort:', answer);
+  //TODO
+};
+
+const nextTask = () => {
+  console.log('Nächste Aufgabe');
   //TODO
 };
 </script>
@@ -14,16 +30,22 @@ const sendAnswer = (answer) => {
   <div class="flex flex-col justify-center items-center h-screen p-4 bg-gray-100">
     <!-- Container für die Rechenaufgabe -->
     <div class="flex justify-center items-center mb-8">
-      <img src="assets/tasks/56/a56l0504.gif" alt="Rechenaufgabe" class="max-w-full h-auto"/>
+      <img src="assets/tasks/56/560000.gif" alt="Rechenaufgabe" class="max-w-full h-auto"/>
     </div>
     <br>
     <!-- Buttons A bis E in einer Reihe -->
     <div class="flex gap-x-4">
       <button v-for="(option, index) in answerOptions" :key="index" @click="sendAnswer(option)"
-              class="bg-red-500 text-white text-lg px-6 py-3 rounded-lg shadow-md hover:bg-red-600 transition duration-200">
+              class="bg-orange-500 text-white text-lg px-6 py-3 rounded-lg shadow-md hover:!bg-orange-600 transition duration-200">
         {{ option }}
       </button>
     </div>
+    <button @click="toggleSolutionTask" class="mt-2 bg-orange-500 text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200">
+      {{ showSolution ? 'Aufgabe' : 'Lösung' }} anzeigen
+    </button>
+    <button @click="nextTask" class="mt-2 bg-orange-500 text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200">
+      Nächste Aufgabe
+    </button>
   </div>
 </template>
 
@@ -45,6 +67,6 @@ button {
 }
 
 button:hover {
-  background-color: #c53030;
+  background-color: rgb(234, 88, 12);
 }
 </style>
