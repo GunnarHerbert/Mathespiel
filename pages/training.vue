6a84730a8f8359f6fe56fd060af6108986c7e8a9
Daniel Wannke Laptop <daniel.wannke@web.de>
2025-04-24 18:40:31 +0200

    style: draft for training page

--- pages/training.vue
+++ pages/training.vue
@@ -78,72 +78,66 @@ var errorMessage = ref("");
       />
 
       <!-- Main-Inhalte -->
-      <div class="absolute top-0 left-0 w-full h-full">
+      <div class="mainContainer absolute top-0 left-0 w-full h-full">
         <a href="/main">
-          <img src="/assets/images/MainMenuButton.jpg" class="mainMenuBtn" alt="Hauptmenü">
+          <img alt="Hauptmenü" class="mainMenuBtn" src="/assets/images/MainMenuButton.jpg">
         </a>
-<!--        <img-->
-<!--            href="/main"-->
-<!--            alt="Hauptmenü"-->
-<!--            class="max-w-[90vw] max-h-[90vh] w-auto h-auto rounded-xl shadow-lg"-->
-<!--            src="/assets/images/MainMenuButton.jpg">-->
-        <!--        </NuxtLink>-->
-        <div>
-          <!--        <div-->
-          <!--            :style="{ visibility: showPointsDelta ? 'visible' : 'hidden' }"-->
-          <!--            class="text-blue-700 text-xl font-bold mt-2">-->
-          <!--          + 10 Kristalle!-->
-          <!--        </div>-->
-          <!--        &lt;!&ndash; Container für die Rechenaufgabe &ndash;&gt;-->
-          <!--        <div class="flex justify-center items-center mb-8 w-full" style="height: 250px;">-->
-          <!--          <img :src="taskImagePath" alt="Rechenaufgabe" class="max-w-full h-4/5 object-contain"/>-->
-          <!--        </div>-->
-          <!--        <div>-->
-          <!--          &lt;!&ndash; Points-Text mit reserviertem Platz &ndash;&gt;-->
-          <!--          <div class="points-text-container">-->
-          <!--            <div :class="{-->
-          <!--             'text-green-600': pointsDelta > 0,-->
-          <!--             'text-red-600': pointsDelta < 0-->
-          <!--           }"-->
-          <!--                 :style="{ visibility: showPointsDelta ? 'visible' : 'hidden' }"-->
-          <!--                 class="text-xl font-bold points-text">-->
-          <!--              {{ pointsDelta > 0 ? '+' : '' }}{{ pointsDelta }} Punkte!-->
-          <!--            </div>-->
-          <!--          </div>-->
-
-          <!--          &lt;!&ndash; Fortschrittsbalken &ndash;&gt;-->
-          <!--          <div class="progress-container">-->
-          <!--            <div :style="{ width: (points / 10000) * 100 + '%' }" class="progress-bar"></div>-->
-          <!--          </div>-->
-
-          <!--          &lt;!&ndash; Antwort-Buttons &ndash;&gt;-->
-          <!--          <div class="flex gap-x-4">-->
-          <!--            <button v-for="(option, index) in answerOptions" :key="index" :class="!isAnswerSent ? 'buttonDefault' :-->
-          <!--                  option === correctAnswerLetter ? 'buttonDisabledCorrect' :-->
-          <!--                  option === userAnswerLetter ? 'buttonDisabledFalse' :-->
-          <!--                  'buttonDisabledDefault'"-->
-          <!--                    :disabled="isAnswerSent"-->
-          <!--                    class="text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200"-->
-          <!--                    @click="sendAnswer(option)">-->
-          <!--              {{ option }}-->
-          <!--            </button>-->
-          <!--          </div>-->
-
-          <!--          &lt;!&ndash; Next Task Button &ndash;&gt;-->
-          <!--          <button class="mt-2 buttonDefault text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200"-->
-          <!--                  @click="nextTask">-->
-          <!--            Nächste Aufgabe-->
-          <!--          </button>-->
-
-          <!--          &lt;!&ndash; Show Solution Button &ndash;&gt;-->
-          <!--          <button :class="isAnswerSent ? 'buttonDefault' : 'buttonDisabledDefault'"-->
-          <!--                  :disabled="!isAnswerSent"-->
-          <!--                  class="mt-2 text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200"-->
-          <!--                  @click="toggleSolutionTask">-->
-          <!--            {{ showSolution ? 'Aufgabe' : 'Lösung' }} anzeigen-->
-          <!--          </button>-->
-          <!--        </div>-->
+        <div
+            :style="{ visibility: showPointsDelta ? 'visible' : 'hidden' }"
+            class="crystalText">
+          + 10 Kristalle!
         </div>
+        <!-- Rechenaufgabe bzw SolutionImage -->
+        <img :src="taskImagePath" alt="Rechenaufgabe" class="taskImg"/>
+        <!-- Points-Text mit reserviertem Platz -->
+        <div class="points-text-container">
+          <div :class="{
+                       'text-green-600': pointsDelta > 0,
+                       'text-red-600': pointsDelta < 0
+                     }"
+               :style="{ visibility: showPointsDelta ? 'visible' : 'hidden' }"
+               class="pointsTxt">
+            {{ pointsDelta > 0 ? '+' : '' }}{{ pointsDelta }} Punkte!
+          </div>
+        </div>
+        <!-- Fortschrittsbalken -->
+        <div
+            :style="{ width: (points / 10000) * 57.6 + '%' }" class="progress-bar"
+        ></div>
+
+        <!-- Antwort-Buttons -->
+<!--        <div class="absolute top-[82%] left-[39%] flex gap-x-[8%] bg-purple-600">-->
+<!--          <button v-for="(option, index) in answerOptions" :key="index" :class="!isAnswerSent ? 'buttonDefault' :-->
+<!--                          option === correctAnswerLetter ? 'buttonDisabledCorrect' :-->
+<!--                          option === userAnswerLetter ? 'buttonDisabledFalse' :-->
+<!--                          'buttonDisabledDefault'"-->
+<!--                  :disabled="isAnswerSent"-->
+<!--                  class="w-[300%] text-white text-lg rounded-lg shadow-md transition duration-200"-->
+<!--                  @click="sendAnswer(option)">-->
+<!--            {{ option }}-->
+<!--          </button>-->
+<!--        </div>-->
+<!--        <client-only>-->
+          <button class="absolute top-[80%] left-[80%] w-10 h-auto bg-purple-600">test</button>
+<!--        </client-only>-->
+        <!-- Next Task Button -->
+<!--        <button-->
+<!--            class="absolute top-[82%] left-[83.5%] w-[10.5%] buttonDefault text-white text-sm rounded-lg shadow-md transition duration-200-->
+<!--         whitespace-nowrap truncate-->
+<!--         flex items-center justify-center text-center font-light"-->
+<!--            @click="nextTask"-->
+<!--        >-->
+<!--          Nächste Aufgabe-->
+<!--        </button>-->
+
+        <!--          &lt;!&ndash; Show Solution Button &ndash;&gt;-->
+        <!--          <button :class="isAnswerSent ? 'buttonDefault' : 'buttonDisabledDefault'"-->
+        <!--                  :disabled="!isAnswerSent"-->
+        <!--                  class="mt-2 text-white text-lg px-6 py-3 rounded-lg shadow-md transition duration-200"-->
+        <!--                  @click="toggleSolutionTask">-->
+        <!--            {{ showSolution ? 'Aufgabe' : 'Lösung' }} anzeigen-->
+        <!--          </button>-->
+        <!--        </div>-->
       </div>
     </div>
   </div>
@@ -154,13 +148,77 @@ var errorMessage = ref("");
 .mainMenuBtn {
   position: absolute;
   right: 4%;
-  /* Abstand oben = 10% der Elternhöhe */
   top: 4%;
-
-  /* Größe relativ zur Eltern-Größe (z. B. 20% Breite & 10% Höhe) */
   width: 11%;
-  /* Optionales Styling */
-  background-color: transparent; /* Tailwind's blue-500 */
+  background-color: transparent;
+}
+
+.crystalText {
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  font-weight: 700;
+  font-size: min(1.641479 * 3vh, 3vw);
+  line-height: min(1.641479 * 2vh, 2vw);
+  color: rgb(29, 78, 216, 1);
+}
+
+.taskImg {
+  position: absolute;
+  top: 26.5%;
+  left: 39.5%;
+  width: 54%;
+  height: auto;
+  border-radius: min(1.641479 * 0.5vh, 0.5vw);
+}
+
+.pointsTxt {
+  position: absolute;
+  top: 84%;
+  left: 71%;
+  font-weight: 400;
+  font-size: min(1.641479 * 2vh, 2vw);
+  line-height: min(1.641479 * 2vh, 2vw);
+}
+
+.progress-bar {
+  position: absolute;
+  top: 91.1%;
+  left: 39.05%;
+  height: 4.1%;
+  border-radius: min(1.641479 * 0.5vh, 0.5vw);
+  background-color: #4caf50;
+  transition: width 0.3s ease-in-out;
+}
+
+/* Stil für die Buttons */
+.answerButton {
+  font-size: 1.25rem;
+  /*width: 60%;*/
+  /*padding: 15% 25%;*/
+  border-radius: 0.375rem;
+}
+
+.buttonDefault {
+  --tw-bg-opacity: 1;
+  background-color: rgb(249 115 22 / var(--tw-bg-opacity, 1));
+}
+
+.buttonDefault:hover {
+  --tw-bg-opacity: 1 !important;
+  background-color: rgb(245 158 11 / var(--tw-bg-opacity, 1)) !important;
+}
+
+.buttonDisabledDefault {
+  background-color: rgb(155, 50, 4);
+}
+
+.buttonDisabledCorrect {
+  background-color: rgb(1, 68, 1) !important;
+}
+
+.buttonDisabledFalse {
+  background-color: rgb(86, 2, 2);
 }
 </style>
 
@@ -185,34 +243,5 @@ var errorMessage = ref("");
 
 <!--.points-text {-->
 <!--  min-height: 30px; /* Fester Platz für den Text */-->
-<!--}-->
-
-<!--/* Optional: Stil für die Buttons */-->
-<!--button {-->
-<!--  font-size: 1.25rem;-->
-<!--  padding: 1rem 2rem;-->
-<!--  border-radius: 0.375rem;-->
-<!--}-->
-
-<!--.buttonDefault {-->
-<!--  &#45;&#45;tw-bg-opacity: 1;-->
-<!--  background-color: rgb(249 115 22 / var(&#45;&#45;tw-bg-opacity, 1));-->
-<!--}-->
-
-<!--.buttonDefault:hover {-->
-<!--  &#45;&#45;tw-bg-opacity: 1 !important;-->
-<!--  background-color: rgb(245 158 11 / var(&#45;&#45;tw-bg-opacity, 1)) !important;-->
-<!--}-->
-
-<!--.buttonDisabledDefault {-->
-<!--  background-color: rgb(155, 50, 4);-->
-<!--}-->
-
-<!--.buttonDisabledCorrect {-->
-<!--  background-color: rgb(1, 68, 1) !important;-->
-<!--}-->
-
-<!--.buttonDisabledFalse {-->
-<!--  background-color: rgb(86, 2, 2);-->
 <!--}-->
 <!--</style>-->
