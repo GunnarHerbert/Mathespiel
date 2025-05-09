fe5dda1e88a957a4e67038c5ea9df1f8c86218a3
yaqed <daniel.wannke@web.de>
2025-05-09 23:29:19 +0200

    run on custom port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
+    devServer: {
+        // port: 3000,
+        // port: 4545,
+        port: 80,
+        host: '0.0.0.0',
+    },
     devtools: {enabled: true},
     nitro: {
         experimental: {
